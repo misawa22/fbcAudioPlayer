9ce66bf77e1ae9f3b91cdf0b6d0d2c98230c63b7
Chad Stout <stoutci@gmail.com>
2016-09-11 16:53:39 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<div class="player">
+    <ul>
+        <li class="cover"><img src="images/logo.png"/></li>
+        <li class="info">
+            <h1>Pastor Rick</h1>
+            <h4>Scripture Here</h4>
+            <h2>How to Reap for a Spiritual Harvest</h2>
+
+            <div class="button-items">
+                <audio id="music" preload="auto" loop="false">
+                    <source src="http://www.friendshipbc.net/uploads/2/6/9/7/2697765/3_15_2015_am_rick_johnson.mp3">
+                    <source src="https://dl.dropbox.com/s/75jpngrgnavyu7f/The-Noisy-Freaks.ogg?dl=1" type="audio/ogg">
+                </audio>
+                <div id="slider"><div id="elapsed"></div></div>
+                <p id="timer">0:00</p>
+                <div class="controls">
+               <span class="expend"><svg class="step-backward" viewBox="0 0 25 25" xml:space="preserve">
+                  <g><polygon points="4.9,4.3 9,4.3 9,11.6 21.4,4.3 21.4,20.7 9,13.4 9,20.7 4.9,20.7"></polygon></g>
+               </svg></span>
+
+                    <svg id="play" viewBox="0 0 25 25" xml:space="preserve">
+                   <defs><rect x="-49.5" y="-132.9" width="446.4" height="366.4"></rect></defs>
+                        <g><circle fill="none" cx="12.5" cy="12.5" r="10.8"></circle>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.7,6.9V18c0,0,0.2,1.4,1.8,0l8.1-4.8c0,0,1.2-1.1-1-2L9.8,6.5 C9.8,6.5,9.1,6,8.7,6.9z"></path>
+                        </g>
+               </svg>
+
+
+               <svg id="pause" viewBox="0 0 25 25" xml:space="preserve">
+                  <g>
+                      <rect x="6" y="4.6" width="3.8" height="15.7"></rect>
+                      <rect x="14" y="4.6" width="3.9" height="15.7"></rect>
+                  </g>
+               </svg>
+
+                    <span class="expend"><svg class="step-foreward" viewBox="0 0 25 25" xml:space="preserve">
+                  <g><polygon points="20.7,4.3 16.6,4.3 16.6,11.6 4.3,4.3 4.3,20.7 16.7,13.4 16.6,20.7 20.7,20.7"></polygon></g>
+                </svg></span>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+<script src="js/main.js"></script>
